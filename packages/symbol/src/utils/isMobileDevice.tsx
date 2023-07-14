19585bd21aa528fdc62176832ecc1c808001e42f
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2023-07-15 01:20:31 +0900

    [add] .gitignore

--- packages/symbol/src/utils/isMobileDevice.tsx
+++ packages/symbol/src/utils/isMobileDevice.tsx
@@ -1,8 +1,8 @@
 export function isMobileDevice(): Boolean {
-    var userAgent = navigator.userAgent;
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
-        return true;
-    } else {
-        return false;
-    }
+  var userAgent = navigator.userAgent;
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+    return true;
+  } else {
+    return false;
+  }
 }
